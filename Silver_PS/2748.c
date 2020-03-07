4292ed5dd31930226478be2e94ffb116c3858063
yujo <yujo@student.42seoul.kr>
2020-03-07 15:04:15 +0900

    solved

--- Silver_PS/2748.c
+++ Silver_PS/2748.c
@@ -1,37 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   10250.c                                            :+:      :+:    :+:   */
+/*   2748.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/06 22:32:37 by yujo              #+#    #+#             */
-/*   Updated: 2020/03/06 22:46:34 by yujo             ###   ########.fr       */
+/*   Created: 2020/03/07 14:50:11 by yujo              #+#    #+#             */
+/*   Updated: 2020/03/07 15:01:27 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
+
+long long dp_fibo(int n)
+{
+	long long arr[99];
+	arr[0] = 0;
+	arr[1] = 1;
+	for (int i = 0; i < n; i++)
+		arr[i + 2] = arr[i + 1] + arr[i];
+
+	return arr[n];
+}
+
 int main(void)
 {
-	int t; // test case
-	int h, w // height, width;
-	int n; // number;
+	int n;
 
-	scanf("%d", &t);
-	for (int i = 0; i < t; i++)
-	{
-		scanf("%d %d %d", &h, &w, &n);
-		int hotel[h][w];
-		for (int i = 1; i <= h; i++) //호텔 방 번호 입력
-		{
-			hotel[i] = i;
-			for (int j = 1; j <= w; j++)
-				hotel[i][j] = j;
-		}
-
-	}
-
+	scanf("%d", &n);
+	printf("%lld", dp_fibo(n));
 
 	return 0;
 }
