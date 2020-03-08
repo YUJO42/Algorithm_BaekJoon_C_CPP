5ad78747df6cc3686ae038329f32b6064b022969
yujo <yujo@student.42seoul.kr>
2020-03-08 14:32:29 +0900

    15059.c

--- Bronze/15059.c
+++ Bronze/15059.c
@@ -6,7 +6,7 @@
 /*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 14:16:20 by yujo              #+#    #+#             */
-/*   Updated: 2020/03/08 14:16:32 by yujo             ###   ########.fr       */
+/*   Updated: 2020/03/08 14:31:10 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 int main(void)
 {
+	int pc, pb, pp;
+	int cc, cb, cp;
+	int sum = 0;
+	
+	scanf("%d %d %d", &pc, &pb, &pp);
+	scanf("%d %d %d", &cc, &cb, &cp);
 
+	if (cc > pc)
+		sum += (cc - pc);
+	if (cb > pb)
+		sum += (cb - pb);
+	if (cp > pp)
+		sum += (cp - pp);
+	printf("%d", sum);
+
+	return 0;
 }
