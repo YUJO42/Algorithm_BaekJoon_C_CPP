eca093a52fc4df89d8624f6626b55a12f42169ac
yujo <yujo@student.42seoul.kr>
2020-03-09 10:23:03 +0900

    subway coding

--- Silver/1934.c
+++ Silver/1934.c
@@ -6,12 +6,32 @@
 /*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/08 18:55:01 by yujo              #+#    #+#             */
-/*   Updated: 2020/03/08 19:25:05 by yujo             ###   ########.fr       */
+/*   Updated: 2020/03/09 10:05:52 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 // 유클리드 호제법
+
+void check(int a, int b)
+{
+	int temp;
+
+	if (a < b)
+	{
+		temp = a;
+		a = b;
+		b = temp;
+	}
+}
+
+int euclid(int a, int b)
+{
+	if (b == 0)
+		return a;
+	euclid(b, a % b);
+}
+
 int main(void)
 {
 	int t;
@@ -21,13 +41,8 @@ int main(void)
 	for (int i = 0; i < t; i++)
 	{
 		scanf("%d %d", &a, &b);
-		while (b)
-		{
-			c = a % b;
-			a = b;
-			b = c;
-		}
-		printf("%d\n", a);
+		check(a, b);
+		printf("%d\n", a * b / euclid(a, b));
 	}
 
 	return 0;
