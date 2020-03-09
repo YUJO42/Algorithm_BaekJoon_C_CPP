32f299989465fe410678f8bc99ce0318ec1d8d3e
yujo <yujo@student.42seoul.kr>
2020-03-09 16:52:36 +0900

    solved

--- Bronze/5597.c
+++ Bronze/5597.c
@@ -1,39 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   11729.c                                            :+:      :+:    :+:   */
+/*   5597.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/08 01:43:07 by yujo              #+#    #+#             */
-/*   Updated: 2020/03/08 02:00:19 by yujo             ###   ########.fr       */
+/*   Created: 2020/03/09 16:05:07 by yujo              #+#    #+#             */
+/*   Updated: 2020/03/09 16:51:03 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <math.h>
-
-void hanoi(int n, int one, int two, int three)
-{
-	if (n == 1)
-		printf("%d %d\n", one, three);
-	else 
-	{
-		hanoi(n - 1, one, three, two);
-		printf("%d %d\n", one, three);
-		hanoi(n - 1, two, one, three);
-	}
 
 int main(void)
 {
-	int n;
+	int student[3222] = {0, };
+	int check;
+	int ret[22222] = {0, };
 
-	scanf("%d", &n);
+	//freopen("input.txt", "r", stdin);
+	for (int i = 1; i <= 28; i++)
+	{
+		scanf("%d", &check);
+		student[check] = 1;
+	}
 
-	int k = pow(2, n) - 1;
+	for (int i = 1; i < 31; i++)
+	{
+		if (student[i] == 0 && ret[0] != 0)
+			ret[1] = i;
+		else if (student[i] == 0)
+			ret[0] = i;
+	}
 
-	printf("%d\n", k);
-	hanoi(n, 1, 2, 3);
+	printf("%d\n", ret[0] < ret[1] ? ret[0] : ret[1]);
+	printf("%d", ret[0] > ret[1] ? ret[0] : ret[1]);
 
 	return 0;
 }
