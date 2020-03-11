9af8ecb750ee758b3c25a94c602970eac5c7dc52
yujo <yujo@student.42seoul.kr>
2020-03-11 22:47:58 +0900

    solved 1316

--- Silver/1316.c
+++ Silver/1316.c
@@ -6,44 +6,36 @@
 /*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/11 10:01:11 by yujo              #+#    #+#             */
-/*   Updated: 2020/03/11 16:38:09 by yujo             ###   ########.fr       */
+/*   Updated: 2020/03/11 22:47:39 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdbool.h>
-#include <string.h>
 
 int main(void)
 {
 	int n;
-	int count = 0;
-	char str[101];
 
+	freopen("input.txt", "r", stdin);
 	scanf("%d", &n);
+	int count = 0;
 
 	for (int i = 0; i < n; i++)
 	{
 		int check[26] = {0, };
-		
+		char str[101] = {0, };
+			
 		scanf("%s", str);
 
-		for (int j = 0; str[j]; j++)
+		for (int j = 0; str[j];)
 		{
-			if (check[str[j] - 'a'] == 1)
-			{
-				check[str[j] - 'a'] += 1;
-				//char chk = str[j];
-				//while (chk != str[j])
-				//	j++;
-			}
-			else if (check[str[j] - 'a'] == 0)
-			{
-				check[str[j] - 'a'] += 1;
-				char chk = str[j];
-				while (chk != str[j])
-					j++;
-			}
+		check[str[j] - 'a'] += 1;
+		char chk = str[j];
+		if (chk == str[j + 1])
+			while (chk == str[j])
+				j++;
+		else
+			j++;
 		}
 		count += 1;
 		for (int k = 0; k < 26; k++)
