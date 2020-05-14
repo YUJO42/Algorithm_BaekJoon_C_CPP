0252ad4ab2eb9c7c912a4a6423a0a44b87ca6043
yujo <yujo@student.42seoul.kr>
2020-05-14 09:30:59 +0900

    daily ps

--- LeetCode/Medium/8_atoi.c
+++ LeetCode/Medium/8_atoi.c
@@ -1,20 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.c                                             :+:      :+:    :+:   */
+/*   8_atoi.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/05/13 18:07:45 by yujo              #+#    #+#             */
-/*   Updated: 2020/05/13 18:08:29 by yujo             ###   ########.fr       */
+/*   Created: 2020/05/13 21:17:56 by yujo              #+#    #+#             */
+/*   Updated: 2020/05/14 09:30:28 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int main(void)
+int myAtoi(char *str)
 {
-	int a = 300;
+	int i = 0;
+	long long sign = 1;
+	long long result = 0;
 
-	printf("%ld", sizeof(a));
+	while (str[i] == ' ')
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while ('0' <= str[i] && str[i] <= '9')
+	{
+		result *= 10;
+		rusilt += str[i] - '0';
+		if (result > 2147483647)
+		{
+			if (sign == 1)
+				return 2147483647;
+			else
+				return -2147483647;
+		}
+		i++;
+	}
+	return (result * sign);
 }
