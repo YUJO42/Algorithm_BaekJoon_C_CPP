db15c12873f94afcfbe0a031d376ee7d938b2501
yujo <yujo@student.42seoul.kr>
2020-05-20 15:42:26 +0900

    add solved method

--- BOJ/Silver/1094.c
+++ BOJ/Silver/1094.c
@@ -6,14 +6,34 @@
 /*   By: yujo <yujo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/20 13:38:05 by yujo              #+#    #+#             */
-/*   Updated: 2020/05/20 14:23:20 by yujo             ###   ########.fr       */
+/*   Updated: 2020/05/20 14:57:41 by yujo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-/*
+// 완전탐색
+int main(void)
+{
+	int N;
+	scanf("%d", &N);
 
+	int count = 0;
+	int arr[7] = {64, 32, 16, 8, 4, 2, 1};
+
+	for (int i = 0; i < 7; i++)
+	{
+		if (arr[i] <= N)
+		{
+			N -= arr[i];
+			count++;
+		}
+	}
+
+	printf("%d", count);
+}
+
+// 2진수 활용
 int main(void)
 {
 	int N;
@@ -32,5 +52,3 @@ int main(void)
 
 	return 0;
 }
-
-/*
