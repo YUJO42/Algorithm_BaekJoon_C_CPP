d6a49a53003677634b6853375ab9ea166f2e9d4a
yujo <yujo@student.42seoul.kr>
2020-06-07 15:53:28 +0900

    2020 06 07 daily ps [JS]Programmers High Score Kit Sort level 1

---
+++ Programmers/High_Score_Kit/sort/k_number.js
@@ -0,0 +1,36 @@
+// Fail ( 85.7 / 100)
+// Test 2 Fail
+function solution(array, commands) {
+  var answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    let result = array.slice(commands[i][0] - 1, commands[i][1]);
+    result.sort((x, y) => x - y);
+    answer.push(result[commands[i][2] - 1]);
+  }
+
+  return answer;
+}
+
+// Success
+function solution(array, commands) {
+  var answer = [];
+
+  for (let i = 0; i < commands.length; i++) {
+    let result = array.slice(commands[i][0] - 1, commands[i][1]);
+    result.sort((x, y) => x - y);
+    answer.push(result[commands[i][2] - 1]);
+  }
+
+  return answer;
+}
+
+// test code
+
+const array = [1, 5, 2, 6, 3, 7, 4];
+const commands = [
+  [2, 5, 3],
+  [4, 4, 1],
+  [1, 7, 3],
+];
+console.log(solution(array, commands)); // output : [5, 6, 3]
