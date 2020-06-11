9313ba2b6e2d3bee06c884e31a87eaa576e1cf56
yujo <yujo@student.42seoul.kr>
2020-06-11 10:05:36 +0900

    build burte force

---
+++ Algorithm_Data_Structure/String/brute_force.js
@@ -0,0 +1,26 @@
+function searchWord(text, word) {
+  let textIndex = 0;
+  let wordIndex = 0;
+  while (text[textIndex] !== undefined) {
+    if (text[textIndex] === word[wordIndex]) {
+      let currentIndex = textIndex;
+      while (text[textIndex] === word[wordIndex]) {
+        if (wordIndex === word.length - 1) {
+          return currentIndex;
+        }
+        textIndex++;
+        wordIndex++;
+      }
+    }
+    wordIndex = 0;
+
+    textIndex++;
+  }
+
+  return -1;
+}
+
+// test code
+
+console.log(searchWord("asdfasdfasfasdfasdfasdfadsfaeeee", "aeeee"));
+console.log(searchWord("asdfasdfasfasdfasdfasdfadsfaeeee", "aeeeeee"));
