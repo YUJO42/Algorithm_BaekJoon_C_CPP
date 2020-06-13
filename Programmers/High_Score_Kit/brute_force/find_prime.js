fa8f51ab0ad81a86af9d6f34d5993110ed44075f
yujo <yujo@student.42seoul.kr>
2020-06-13 11:30:42 +0900

    2020 06 13 daily ps [JS]Brute-Force

---
+++ Programmers/High_Score_Kit/brute_force/find_prime.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  var answer = 0;
+
+  const input = numbers.split("");
+  for (let i = 0; i < input.length; i++) {}
+  return answer;
+}
+
+// test code
+
+console.log(solution("17")); // output : 3
+console.log(solution("011")); // output : 2
