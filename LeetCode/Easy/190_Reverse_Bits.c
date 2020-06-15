dab5e83bfc2496fdf2174434dd1da49397a89e6e
yujo <yujo@student.42seoul.kr>
2020-06-15 12:24:09 +0900

    2020 06 15 daily ps [JS]Stirng

--- LeetCode/Easy/190_Reverse_Bits.c
+++ LeetCode/Easy/190_Reverse_Bits.c
@@ -13,16 +13,16 @@
 #include <stdio.h>
 #include <stdint.h>
 
-uint32_t reverseBits(uint32_t n) 
+uint32_t reverseBits(uint32_t n)
 {
-    uint32_t reverse_n = 0x00000000;
+	uint32_t reverse_n = 0x00000000;
 
-    for (int i = 0; i < 32; i++)
-    {
-        reverse_n = (n & 1) + (reverse_n << 1);
-        n >>= 1;
-    }
-    return reverse_n;
+	for (int i = 0; i < 32; i++)
+	{
+		reverse_n = (n & 1) + (reverse_n << 1);
+		n >>= 1;
+	}
+	return reverse_n;
 }
 
 void print_binary(int x, int y)
@@ -37,13 +37,13 @@ int main(void)
 {
 	uint32_t test1 = 0b00000010100101000001111010011100;
 	uint32_t test2 = 0b11111111111111111111111111111101;
-    
+
 	printf("test1 output	: 00111001011110000010100101000000\n");
-	printf("test1 my output : "); 
+	printf("test1 my output : ");
 	print_binary(reverseBits(test1), 32);
 	printf("\n");
 	printf("test2 output	: 10111111111111111111111111111111\n");
-	printf("test2 my output : "); 
+	printf("test2 my output : ");
 	print_binary(reverseBits(test2), 32);
 	printf("\n");
 
---
+++ LeetCode/Medium/1456_Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.js
@@ -0,0 +1,92 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+
+// solution 1 : Brute-Force
+// result     : Time Limit Exceeded
+
+// var maxVowels = function (s, k) {
+//   const arr = ["a", "e", "i", "o", "u"];
+//   let max = 0;
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (arr.indexOf(s[i] !== -1)) {
+//       let count = 0;
+//       for (let j = 0; j < k && i + j < s.length; j++) {
+//         if (arr.indexOf(s[i + j]) !== -1) {
+//           count++;
+//         }
+//       }
+//       if (count > max) {
+//         max = count;
+//       }
+//     }
+//   }
+// return max;
+
+// };
+
+// solution 2 : Brute-Force (add conditon)
+// result     : Time Limit Exceeded
+
+// var maxVowels = function (s, k) {
+//   if (s.length === 0) {
+//     return 0;
+//   }
+//   const arr = ["a", "e", "i", "o", "u"];
+//   let max = 0;
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (max === k) {
+//       break;
+//     }
+//     if (arr.indexOf(s[i] !== -1)) {
+//       let count = 0;
+//       for (let j = 0; j < k && i + j < s.length; j++) {
+//         if (arr.indexOf(s[i + j]) !== -1) {
+//           count++;
+//         }
+//       }
+//       if (count > max) {
+//         max = count;
+//       }
+//     }
+//   }
+
+//   return max;
+// };
+
+// solution 3 :
+// result     :
+
+var maxVowels = function (s, k) {
+  const arr = ["a", "e", "i", "o", "u"];
+  let max = 0;
+  let count = 0;
+  for (let i = 0, count = 0; i < s.length; i++) {
+    if (arr.indexOf(s[i]) !== -1) {
+      count++;
+    }
+    if (i >= k && arr.indexOf(s[i - k]) !== -1) {
+      count--;
+    }
+    max = Math.max(count, max);
+  }
+  return max;
+};
+
+// test code
+
+const s1 = "abciiidef";
+const s2 = "aeiou";
+const s3 = "leetcode";
+const s4 = "rhythms";
+const s5 = "tryhard";
+
+console.log(maxVowels(s1, 3)); // output : 3
+console.log(maxVowels(s2, 2)); // output : 2
+console.log(maxVowels(s3, 3)); // output : 2
+console.log(maxVowels(s4, 4)); // output : 0
+console.log(maxVowels(s5, 4)); // output : 1
