60f995ec51e65bb22e986e212ceacfb0bf066f24
yujo <yujo@student.42seoul.kr>
2020-06-17 12:44:42 +0900

    modified Rabin-Karp Algorithm

--- Algorithm_Data_Structure/String/Rabin-Karp.js
+++ Algorithm_Data_Structure/String/Rabin-Karp.js
@@ -1,44 +1,80 @@
-// simply
+// // simply
 
-function rabinKarp(text, word) {
-  const textHash = [];
-  const wordHash = [];
+// function rabinKarp(text, word) {
+//   const textHash = [];
+//   const wordHash = [];
 
-  for (let i = 0; i < text.length; i++) {
-    if (i === 0) {
-      for (let j = 0; j < word.length; j++) {
-        textHash.push(text[j]);
-        wordHash.push(word[j]);
-      }
-    } else {
-      textHash.shift();
-      textHash.push(text[i + word.length - 1]);
-    }
+//   for (let i = 0; i < text.length; i++) {
+//     if (i === 0) {
+//       for (let j = 0; j < word.length; j++) {
+//         textHash.push(text[j]);
+//         wordHash.push(word[j]);
+//       }
+//     } else {
+//       textHash.shift();
+//       textHash.push(text[i + word.length - 1]);
+//     }
 
-    if (textHash.join("") === wordHash.join("")) {
-      return i;
-    }
-  }
+//     if (textHash.join("") === wordHash.join("")) {
+//       return i;
+//     }
+//   }
 
-  return -1;
-}
+//   return -1;
+// }
 
-// 원래 버전
+// // 원래 버전
+
+// function rabinKarp(text, word) {
+//   let textHash = 0;
+//   let wordHash = 0;
+//   const prime = 3;
+
+//   for (let i = 0; i < text.length; i++) {
+//     if (i == 0) {
+//       for (let j = 0; j < word.length; j++) {
+//         textHash += text.charCodeAt(j) * prime;
+//         wordHash += word.charCodeAt(j) * prime;
+//       }
+//     } else {
+//       textHash -= text.charCodeAt(i) * prime;
+//       textHash += text.charCodeAt(i + word.length - 1) * prime;
+//     }
+//     if (textHash === wordHash) {
+//       let find = true;
+//       for (let j = i; j < word.length; j++) {
+//         if (text[j] !== word[j]) {
+//           find = false;
+//           break;
+//         }
+//       }
+//       if (find) {
+//         return i;
+//       }
+//     }
+//   }
+
+//   return -1;
+// }
 
 function rabinKarp(text, word) {
   let textHash = 0;
   let wordHash = 0;
-  const prime = 3;
+  let power = 1;
 
   for (let i = 0; i < text.length; i++) {
     if (i == 0) {
       for (let j = 0; j < word.length; j++) {
-        textHash += text.charCodeAt(j) * prime;
-        wordHash += word.charCodeAt(j) * prime;
+        textHash += text.charCodeAt(text.length - 1 - j) * power;
+        wordHash += word.charCodeAt(word.length - 1 - j) * power;
+        if (j < text.length - 1) {
+          power *= 2;
+        }
       }
     } else {
-      textHash -= text.charCodeAt(i) * prime;
-      textHash += text.charCodeAt(i + word.length - 1) * prime;
+      textHash =
+        2 * (textHash - text.charCodeAt(i - 1) * power) +
+        text[word.length - 1 - i];
     }
     if (textHash === wordHash) {
       let find = true;
