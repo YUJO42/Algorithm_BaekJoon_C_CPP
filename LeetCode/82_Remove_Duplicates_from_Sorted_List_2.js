e5ef45113d15cdc7810c8b671720d63bbe48820c
yujo <yujo@student.42seoul.kr>
2020-06-19 15:41:29 +0900

    2020 06 19 daily ps [JS]Linked List

---
+++ LeetCode/82_Remove_Duplicates_from_Sorted_List_2.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+var deleteDuplicates = function (head) {
+  let current = head.next;
+  let temp = new ListNode();
+
+  return head;
+};
---
+++ LeetCode/Easy/203_remove_linked_list_elements.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, hit) {
+  var previous = new ListNode(-1);
+  var current = previous;
+
+  previous.next = head;
+
+  while (current.next) {
+    if (current.next.val === val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return previous.next;
+};
