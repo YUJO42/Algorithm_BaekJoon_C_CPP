0503a38d78f10768c79576076fef6bf405ac6adb
yujo <yujo@student.42seoul.kr>
2020-07-04 10:39:01 +0900

    2020 07 04 daily ps [JS]string

---
+++ Programmers/level_01/strings_sort_suibian.js
@@ -0,0 +1,11 @@
+function solution(strings, n) {
+  return strings.sort((x, y) => {
+    if (x[n] > y[n]) {
+      return 1;
+    } else if (x[n] < y[n]) {
+      return -1;
+    } else {
+      return x.localeCompare(y);
+    }
+  });
+}
