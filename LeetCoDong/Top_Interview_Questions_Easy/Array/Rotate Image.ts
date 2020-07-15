31b67101c49ed897bffca917becfa74dcf2b7573
JO YUN HO <bedro27@gmail.com>
2020-07-15 17:19:30 +0900

    2020 07 15 daily ps [TS]Array

--- LeetCoDong/Top_Interview_Questions_Easy/Array/Rotate Image.ts
+++ LeetCoDong/Top_Interview_Questions_Easy/Array/Rotate Image.ts
@@ -1,9 +1,45 @@
 /**
  Do not return anything, modify matrix in-place instead.
  */
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
 function rotate(matrix: number[][]): void {
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i + 1; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < Math.floor(matrix.length / 2); j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[i][matrix[i].length - j - 1];
+      matrix[i][matrix[i].length - j - 1] = temp;
+    }
+  }
+}
 
-};
+// test code
+
+console.log(
+  rotate([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
+
+console.log(
+  rotate([
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16],
+  ])
+);
+
 
 // test code
 
