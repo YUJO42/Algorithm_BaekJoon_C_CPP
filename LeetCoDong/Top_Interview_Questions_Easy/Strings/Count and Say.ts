e1b3ce54f7bfb4c773c3f69383877b7fbe1af07f
yujo <yujo@student.42seoul.kr>
2020-07-16 10:36:13 +0900

    2020 07 16 daily ps [TS]String

--- LeetCoDong/Top_Interview_Questions_Easy/Strings/Count and Say.ts
+++ LeetCoDong/Top_Interview_Questions_Easy/Strings/Count and Say.ts
@@ -1,1 +1,25 @@
-function countAndSay(n: number): string {}
+function countAndSay(n: number): string {
+  let result: string = "1";
+  console.log(n);
+  while (n > 1) {
+    let count = 1;
+    let current = "";
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] === result[i + 1]) {
+        count++;
+      } else {
+        current += `${count}${result[i]}`;
+        count = 1;
+      }
+    }
+    n--;
+    console.log(n);
+    result = current;
+  }
+  return result;
+}
+
+// test code
+
+console.log(countAndSay(1)); // 1
+console.log(countAndSay(4)); // 1211
